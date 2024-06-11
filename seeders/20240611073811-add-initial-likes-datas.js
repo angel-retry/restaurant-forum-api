50ea863115f5b159e225f3be2b01e4fc1613cd67
unknown <a27657060@gmail.com>
2024-06-11 15:43:35 +0800

    feat: add initial likes data

---
+++ seeders/20240611073811-add-initial-likes-datas.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const users = await queryInterface.sequelize.query('SELECT id FROM Users WHERE is_admin = false', {
+      type: queryInterface.sequelize.QueryTypes.SELECT
+    })
+
+    const Restaurants = await queryInterface.sequelize.query('SELECT id FROM Restaurants', {
+      type: queryInterface.sequelize.QueryTypes.SELECT
+    })
+
+    await queryInterface.bulkInsert('Likes', Array.from({ length: 15 }, () => ({
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      restaurant_id: Restaurants[Math.floor(Math.random() * Restaurants.length)].id,
+      created_at: new Date(),
+      updated_at: new Date()
+    })))
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Likes', null)
+  }
+}
